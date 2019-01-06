19eb7cd4e986ad9818d65fb4eb4ff41b910e1c58
Joyce Perez <japerez@gmail.com>
2019-01-06 22:48:53 +0800

    added stripe library

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
+session_start();
+
 define('BASE_URL', 'http://localhost/tuitt/cp2-ecommerce');
 // define ('DEFAULT_PROFILE' , 'https://png.pngtree.com/svg/20161021/user_avatar_35720.png');
 define ('DEFAULT_PROFILE' , BASE_URL."/app/assets/images/user_default_img.png" );
 define ('DEFAULT_PRODUCT_IMG' , 'https://via.placeholder.com/350x25');
 date_default_timezone_set('Asia/Manila');
-session_start();
+
+require_once('vendor/autoload.php');
