309544825b0588402b128c1c025ec3b9c8dc3c8c
japerez <japerez.ph@gmail.com>
2019-01-08 14:38:25 +0800

    edits

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -2629,7 +2629,7 @@ $(document).ready( () => {
 					// $("#product_brand").val(dataFromPHP.brand);
 					$("#new_product_id").val(dataFromPHP.id);
 					alert("Saved!");
-					window.location.reload();
+					// window.location.reload();
 				}
 			});
 		}
