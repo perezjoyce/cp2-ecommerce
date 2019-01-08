da7a1c6061e63bbb973b3d42a539c9cf87468218
japerez <japerez.ph@gmail.com>
2019-01-08 20:16:03 +0800

    edits

--- app/partials/templates/cart_modal.php
+++ app/partials/templates/cart_modal.php
@@ -3,4 +3,5 @@
     require_once '../../../config.php';
     require_once BASE_DIR . '/app/sources/pdo/src/PDO.class.php';
     require_once '../../controllers/functions.php';
+    require_once '../../controllers/connect.php';
     echo "test";
