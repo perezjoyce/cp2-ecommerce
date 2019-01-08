e3df7d3acf7612432a04856bc776c91668d26c50
japerez <japerez.ph@gmail.com>
2019-01-08 17:15:44 +0800

    db errors

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -3,10 +3,10 @@
 	require_once '../sources/pdo/src/PDO.class.php';
 
 	//set values
-	$host = "localhost";
-	$db_username = "id8404897_db_demostorenew";
-	$db_password = "";
-	$db_name = "id8404897_db_demostorenew";
+	$host = "db4free.net";
+	$db_username = "japerez";
+	$db_password = "16b786e8";
+	$db_name = "db_demostorenew";
 
 	$conn = new PDO("mysql:host=$host;dbname=$db_name",$db_username,$db_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
