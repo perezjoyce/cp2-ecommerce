bcead5fdc614bb3afc4264dedf5b1639a5ad7fd3
japerez <japerez.ph@gmail.com>
2019-01-10 02:03:31 +0800

    move to dbjaws server db

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ require_once BASE_DIR . '/app/controllers/functions.php';
 require_once BASE_DIR . '/app/sources/pdo/src/PDO.class.php';
 
 //set values
-$host = "db4free.net";
-$db_username = "japerez";
-$db_password = "16b786e8";
-$db_name = "db_demostorenew";
+$host = "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_username = "jian4ja40d0tnwib";
+$db_password = "dlri4m7vxgpl7w0f";
+$db_name = "p4877qn7awrf6o0z";
 
 $conn = new PDO("mysql:host=$host;dbname=$db_name",$db_username,$db_password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
