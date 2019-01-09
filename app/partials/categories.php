4f8e702beb3aff1af241d150ba5882c37ce76e6d
japerez <japerez.ph@gmail.com>
2019-01-09 10:33:48 +0800

    removed require once in category

--- app/partials/categories.php
+++ app/partials/categories.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../../config.php';
+// require_once '../../../config.php';
 ?>
     <!-- CATEGORIES: FOR INDEX AND CATALOG PAGE ONLY -->
     <div class="container-fluid border-bottom white-bg">
