0481e6712e448583b48b6ab565cb83338f095f14
japerez <japerez.ph@gmail.com>
2019-01-13 08:58:31 +0800

    font-size edits

--- app/views/product.php
+++ app/views/product.php
@@ -437,10 +437,10 @@ if(isset($_SESSION['id'])) {
 
           <!-- SELLER DETAILS -->
           <div class="col-lg-2 col-md-3 col-sm-12 white-bg py-5">
-            <div class="container-fluid">
+            <div class="container-fluid px-0">
 
-              <div class='row mb-4'>
-                  <div class="col-12 py-5 border">
+              <div class='row mb-4 border'>
+                  <div class="col-12 py-5 d-flex- flex-column justify-content-center text-center">
                     
                         <?php
                           $sql = "SELECT * FROM tbl_stores WHERE id = ?";
@@ -456,33 +456,26 @@ if(isset($_SESSION['id'])) {
                         ?>
                       
                       <!-- STORE LOG -->
-                      <div class="row justify-content-center mb-4">
-                        <div class="col">
-                          <img src="<?= BASE_URL ."/". $storeLogo . ".jpg"?>" alt="<?=$storeName?>" style='width:70px;max-height:70px;' class='circle'>
-                        </div>
+                      <div class="flex-fill mb-4">
+                        <img src="<?= BASE_URL ."/". $storeLogo . ".jpg"?>" alt="<?=$storeName?>" style='width:70px;max-height:70px;' class='circle'>
                       </div>
 
                       <!-- STORE NAME -->
-                      <div class="row justify-content-center text-purple font-weight-bold mb-4">
-                        <div class="col">
+                      <div class="flex-fill text-purple font-weight-bold mb-4">
                           <?=$storeName?>
-                        </div>
                       </div>
 
                       <!-- STORE ADDRESS -->
-                      <div class="row justify-content-center text-gray mb-2 px-2">
-                        <div class="col">
+                      <div class="flex-fill text-gray mb-2 px-2">
                           <small>
                             <i class="fas fa-map-marker-alt"></i>
                             &nbsp;<?= ucwords(strtolower($storeAddress)) ?>
                           </small>
-                        </div>
                       </div>
 
 
                       <!-- LAST ACTIVITY -->
-                      <div class="row justify-content-center text-gray">
-                        <div class="col">
+                      <div class="flex-fill text-gray">
                           <?php
                             $sql = "SELECT last_login FROM tbl_users WHERE id = ?";
                             $statement = $conn->prepare($sql);
@@ -492,30 +485,29 @@ if(isset($_SESSION['id'])) {
 
                           ?>
                           <small id='lastLoginTimeAgo'><?= $lastLogin ?></small>
-                        </div>
                       </div>
 
                     
                       <hr class='my-4'>
 
                       <!-- STATS -->
-                      <div class="row text-gray mb-4">
-                        <div class="col ml-3">
+                      <div class="flex-fill text-gray mb-4 text-left">
+                       
 
                           <!-- RATINGS -->
                           <div class="d-flex flex-row mb-3">
                               <?php
-                              $sql = "SELECT i.id, i.store_id, AVG(product_rating) as 'averageRating' 
-                              FROM tbl_ratings 
-                              LEFT JOIN tbl_items i ON product_id = i.id 
-                              WHERE store_id = ? GROUP BY product_id";
-                                $statement = $conn->prepare($sql);
-                                $statement->execute([$storeId]);
-                                $row = $statement->fetch();
-                                $averageSellerRating = $row['averageRating'];	
-                                $averageSellerRating = round($averageSellerRating,1);
+                                $sql = "SELECT i.id, i.store_id, AVG(product_rating) as 'averageRating' 
+                                        FROM tbl_ratings 
+                                        LEFT JOIN tbl_items i ON product_id = i.id 
+                                        WHERE store_id = ? GROUP BY product_id";
+                                          $statement = $conn->prepare($sql);
+                                          $statement->execute([$storeId]);
+                                          $row = $statement->fetch();
+                                          $averageSellerRating = $row['averageRating'];	
+                                          $averageSellerRating = round($averageSellerRating,1);
                               ?>
-                            <small style='width:55%;'>
+                            <small style='width:50%;'>
                               Rating
                             </small>
                             <small>
@@ -527,7 +519,7 @@ if(isset($_SESSION['id'])) {
 
                           <!-- FOLLWERS -->
                           <div class="d-flex flex-row mb-3">
-                            <small style='width:55%;'>
+                            <small style='width:50%;'>
                               Followers
                             </small>
                             <small>
@@ -544,7 +536,7 @@ if(isset($_SESSION['id'])) {
                                 $row = $statement->fetch();
                                 $productCount = $row['productCount'];	
                               ?>
-                            <small style='width:55%;'>
+                            <small style='width:50%;'>
                               Products
                             </small>
                             <small>
@@ -555,7 +547,7 @@ if(isset($_SESSION['id'])) {
                           <!-- JOINED -->
                           <div class="d-flex flex-row mb-4">
                               
-                            <small style='width:55%;'>
+                            <small style='width:50%;'>
                               Joined
                             </small>
                             <small>
@@ -564,18 +556,18 @@ if(isset($_SESSION['id'])) {
                             <!-- https://stackoverflow.com/questions/6823133/how-to-remove-first-word-from-a-php-string -->
                           </div>
 
-                        </div> 
+                      
                       </div>
 
                     
                       <!-- BUTTONS -->
-                      <div class="row">
+                      <div class="flex-fill border">
                         <!-- <a href='#' class='btn btn-block border text-purple mx-3 mb-2 py-2 modal-link' id="chatbox" data-url="../partials/templates/chatbox_modal.php">
                           <i class="far fa-comment"></i>
                           &nbsp;Message Seller
                         </a> -->
                         <div class="col">
-                          <a href='store-profile.php?id=<?=$storeId?>' class='btn btn-block border text-secondary mx-3 py-2'>
+                          <a href='store-profile.php?id=<?=$storeId?>' class='btn btn-block text-secondary px-0'>
                             <i class="fas fa-store"></i>
                             &nbsp;View Shop
                           </a>
@@ -586,14 +578,14 @@ if(isset($_SESSION['id'])) {
               </div>
 
               <div class='row border'>
-                <div class="col">
+                
               
                   <a href='#' class='btn btn-block text-gray mx-3 py-2'>
                     <i class="far fa-flag"></i>
                       &nbsp;Report
                   </a>
 
-                </div>
+           
               </div>
 
             </div>
@@ -602,17 +594,17 @@ if(isset($_SESSION['id'])) {
         
 
           <!-- NAV TABS FOR  -->
-          <div class="col-lg-9 col-md-9 col-sm-12">
+          <div class="col">
             <div class="row">
               <div class="col-lg-12">
                 <div class="container-fluid">
                 
                   <!-- TABS -->
-                  <div class="row my-lg-5 mt-md-5">
+                  <div class="row my-lg-5 mt-md-5 no-gutters">
                     <div class="col-12">
                       <div class="tab border-bottom d-flex flex-row">
                         <button class="tablinks flex-fill active" onclick="window.openTab(event, 'info_content')">
-                          <h4><span class='vanish-sm'>Product&nbsp;</span>Details</h4>
+                          <h4><span class='vanish-sm vanish-md'>Product&nbsp;</span>Details</h4>
                         </button>
                         <button class="tablinks flex-fill progress-x" onclick="window.openTab(event, 'reviews_content')">
                           <h4>Reviews</h4>
@@ -625,12 +617,12 @@ if(isset($_SESSION['id'])) {
                   </div>
 
                   <!-- CONTENT -->
-                  <div class='row'>
-                    <div class='col-12'>
+                  <div class='row no-gutters'>
+                    <div class='col-12 px-lg-5 px-md-5 px-sm-0'>
 
                       <!-- PRODUCT INFO -->
                       <div id="info_content" class="tabcontent" style='display:block'>
-                        <div class="container mt-2 pl-0">
+                        <div class="container mt-2 px-0">
                           <div class="row">
                             <!-- <div class="col-1"></div> -->
                             <div class="col">
