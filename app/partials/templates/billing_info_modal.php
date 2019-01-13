81d035f5b3dd108e3b2c668e1599d7098c0810a0
japerez <japerez.ph@gmail.com>
2019-01-13 23:45:43 +0800

    dasdsad

--- app/partials/templates/billing_info_modal.php
+++ app/partials/templates/billing_info_modal.php
@@ -331,7 +331,7 @@
                              <div class="form-group row mb-5">
                                 <label for='modeOfPayment' class='col'>Payment Mode*</label>
                                 <div class="input-group col-9">
-                                    <select class="custom-select" id="modeOfPayment" onchange="modeOfPayment()">
+                                    <select class="custom-select" id="modeOfPayment" onchange="window.modeOfPayment()">
                                         <option value='...' selected="...">...</option>
                                             <?php 
                                                 $sql = " SELECT * FROM tbl_payment_modes ";
@@ -349,15 +349,8 @@
                                 </div>
                             </div>
 
-
-                            
-                                
-
                             <p id="billing_info_error"></p>
-
                             
-                            
-
                             <div class="container px-0 mb-5" id='billing_info_buttons'>
                                 <!-- CHECKOUT BUTTON -->
                                 <div class="row">
