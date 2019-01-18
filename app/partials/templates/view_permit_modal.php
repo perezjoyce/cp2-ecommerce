7472914a0c00e566cdfa2322f9e115c98b1752af
japerez <japerez.ph@gmail.com>
2019-01-18 14:49:25 +0800

    permit

--- app/partials/templates/view_permit_modal.php
+++ app/partials/templates/view_permit_modal.php
@@ -24,7 +24,7 @@
 <div class="container-fluid" id='view_shop_modal'>
     <div class="row">
         
-        <div class="col" style='height:80vh;overflow-y:auto;'>
+        <div class="col" style='height:80vh;overflow-y:auto;' id='printThis'>
 
             <div class="row float-right">
                 <button id='close_modal' type="button" class="close mr-3 mt-2" data-dismiss="modal" aria-label="Close">
@@ -32,7 +32,7 @@
                 </button>
             </div>
 
-            <div class="container p-0 my-lg-5 mt-md-5" id='store_page_container' id='printThis'>
+            <div class="container p-0 my-lg-5 mt-md-5" id='store_page_container'>
 
                 <div class="row mx-0">
                     <div class="col my-5">
