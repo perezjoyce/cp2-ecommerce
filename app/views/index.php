e25853a48376e4e4f6b10ab498e49640cd165553
japerez <japerez.ph@gmail.com>
2019-01-20 00:02:25 +0800

    ads

--- app/views/index.php
+++ app/views/index.php
@@ -148,9 +148,154 @@
     <br class='vanish-sm'>
 
    
-    <!-- VIEWED PRODUCTS, WISHLIST ITEMS AND CART ITEMS THAT WERE NOT PURCHASED, IF LOGGED IN OR SESSION IS NOT DESTROYED -->
-   
+    <!-- CART ITEMS THAT WERE NOT PURCHASED, ORDERED BY THE NUMBER OF TIMES THEY WERE PUT IN CART -->
+    <?php if(isset($_SESSION['id'])){ 
+      
+      $sql = "SELECT c.variation_id, i.id, 
+        COUNT(i.id) 
+        AS numberOfTimesInCart, i.name, i.price, pi.url 
+        FROM tbl_carts c 
+        JOIN tbl_variations v 
+        ON c.variation_id=v.id 
+        JOIN tbl_items i 
+        ON v.product_id=i.id 
+        JOIN tbl_product_images pi 
+        ON pi.product_id=i.id  
+        WHERE status_id IS NULL 
+        AND user_id =? 
+        AND is_primary = 1 
+        GROUP BY i.id 
+        ORDER BY numberOfTimesInCart 
+        DESC 
+        LIMIT 4";
+        $statement = $conn->prepare($sql);
+        $statement->execute([$_SESSION['id']]);
+        //$result = mysqli_query($conn,$sql);
 
+      //CHECK IF THERE'S DATA
+      if($statement->rowCount()){
+    ?>
+    <div class="container mb-5">
+
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-8 pr-0">
+            <h5>
+              FOR YOU
+            </h5>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-4 text-right pt-2">
+          <a href="catalog.php?id=<?=$_GET['id'] = 41 ?>">
+            View All&nbsp;<i class="fas fa-angle-double-right"></i>
+          </a>
+        </div>
+      </div>
+
+      <div class="row no-gutters">
+        <?php 
+            //CREATE A ROW VARIABLE
+            while($row = $statement->fetch()){
+              $productId = $row['id'];
+              $name = $row['name'];
+              $price = $row['price'];
+              $logo = $row['url'];
+              $logo = BASE_URL . "/".$logo .".jpg";
+          ?>
+          <div class="col-lg-3 col-md-4 col-sm-6 pb-2 px-1">
+            <a href="product.php?id=<?= $productId ?>">
+              <div class = 'card h-700 border-0'>
+                <!-- IMGS WON'T RESPOND TO CSS TARGETING ... -->
+                <img class='card-img-top card-profilepic' src="<?= $logo ?>"> 
+                <div class="card-body pr-0">
+                  <div class='font-weight-bold'>
+                    <?= $name ?>
+                  </div>
+                  <div>&#36; <?= $price ?> </div>
+
+                  <div class='d-flex flex-row mt-3'>
+                 
+                    <!-- WISHLIST BUTTONS -->
+                    <div class='flex-fill' style='cursor:default;'>
+
+                      <?php 
+                        $wishCount = getProductWishlishtCount($conn,$productId);
+                        if(isset($_SESSION['id'])) {
+                            if (checkIfInWishlist($conn,$id)) {
+                      ?>
+                        <a class='heart-toggler' data-id='<?= $productId ?>' role='button' data-enabled="0" style='float:left'>
+                          <span class='wish_heart'><i class='fas fa-heart text-purple' id></i></span>
+                          <span class='product_wish_count'>
+                            <small>
+                              <?= $wishCount ?>
+                            </small>
+                          </span>
+                        </a>
+                  
+                      <?php  } else { ?>
+
+                        <a class='heart-toggler' data-id='<?= $productId ?>' data-enabled="1" style='float:left'>
+                          <span class='wish_heart'><i class='far fa-heart text-purple'></i></span> 
+                          <span class='product_wish_count'>
+                            <small>
+                              <?php
+                               if($wishCount == 0){
+                                 echo "";
+                               } else {
+                                 echo $wishCount;
+                               }
+                              ?>
+                            </small>
+                          </span>
+                        </a>
+
+                      <!-- IF LOGGED OUT -->
+                      <?php }  } else { 
+                       
+                        if($wishCount >= 1) {
+                      ?>
+                        
+                        <a class='btn_wishlist_logout_view' data-id='<?= $productId ?>' disabled style='cursor:default; float:left'>
+                          <i class='far fa-heart text-purple'></i> 
+                          <span class='product_wish_count'>
+                            <small>
+                              <?= $wishCount ?>
+                            </small>
+                          </span>
+                        </a>
+                        
+                      <?php } else { ?>
+                        <a class='btn_wishlist_logout_view' data-id='<?= $productId ?>' disabled style='cursor:default; float:left'>
+                          <i class='far fa-heart text-gray'></i> 
+                        </a>
+                        
+                      <?php } } ?>
+                    </div>
+
+                    <!-- AVERAGE STAR RATING -->
+                    <div class='flex-fill pr-3' style="display:flex; flex-direction: column; width:81%; align-items:flex-end">  
+                      <div class='stars-outer' 
+                        data-productrating='<?=getAveProductReview($conn, $productId)?>' 
+                        data-productid='<?=$productId?>' 
+                        id='average_product_stars2<?=$productId?>'>
+                        <span class='stars-inner'></span>
+                      </div>
+                    </div>
+                    <!-- /AVERAGE STAR RATING -->
+                  </div>
+
+                </div>
+                <!-- /.CARD BODY -->
+              </div>
+              <!-- /.CARD -->
+            </a>
+          </div>
+              
+        <?php } ?>
+      </div>
+
+    </div>
+    <?php } } ?>
+   
+    <br class='vanish-sm'>
     <!-- TRENDING PRODUCTS: SHOW TRENDING PRODUCTS BASED ON THE NUMBER OF TIMES PEOPLE ADDED THEM TO THEIR CART AND PROCEEDED TO CHECKOUT -->
     <div class="container mb-5">
 
@@ -300,7 +445,7 @@
       <div class="row">
         <div class="col"></div>
         <div class="col-lg-4 col-md-6">
-          <a href="catalog.php?id=<?=$_GET['id'] = 1 ?>" class="btn btn-lg btn-block border hover_btn">
+          <a href="catalog.php?id=<?=$_GET['id'] = 41 ?>" class="btn btn-lg btn-block border hover_btn">
             See More Products
           </a>
         </div>
