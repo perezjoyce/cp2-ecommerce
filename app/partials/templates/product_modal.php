ce0446a5eef3a42573075f0411ffe6f1cfbf0213
japerez <japerez.ph@gmail.com>
2019-01-24 11:13:21 +0800

    sort ratings fix

--- app/partials/templates/product_modal.php
+++ app/partials/templates/product_modal.php
@@ -1249,7 +1249,10 @@ if(isset($_SESSION['id'])) {
                                   ?>
                       
                                   <div class='test-container' data-rating="<?= $clientRating ?>" data-id='<?= $clientId ?>'></div>
-                                  
+                                  <div class="ratings">
+                                    <div class="empty-stars"></div>
+                                    <div class="full-stars" style="width:<?= (parseInt($clientRating)/5)*100?>%"></div>
+                                  </div>
                                 
                                   <?php } else { ?>
                                 
